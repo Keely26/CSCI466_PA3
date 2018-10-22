b5dd9874ddf9846224f3af40b22e6edf9df4c930
AlexHarry17 <alex.harry1@gmail.com>
2018-10-21 20:53:04 -0600

    Fixed error with splitting message as the length was incorrect.  Changed the message to make it 85 characters. Change network.py and link.py to _1.py and _2.py

--- simulation_1.py
+++ simulation_1.py
@@ -46,11 +46,11 @@ if __name__ == '__main__':
 
     # create some send events
     for i in range(3):
-        message = 'this is data message %d, this message is at least 80 characters long that needs to be split' % i
+        message = 'this is data message %d, this message is at least 80 characters long, needs to be split' % i
         if len(message) > 50:
             message_1 = message[0:44]
             client.udt_send(2, message_1)
-            message_2 = message[50:99]
+            message_2 = message[44:89]
             client.udt_send(2, message_2)
         else:
             client.udt_send(2, message)
